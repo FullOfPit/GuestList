42e40ee3666aa44a21b8645cac53278835f63c46
Christian Weck <weck.chr@gmail.com>
2022-11-30 20:05:31 +0100

    Added functionality that writes/reads from guests.txt

--- src/main/java/GuestList.java
+++ src/main/java/GuestList.java
@@ -1,2 +1,52 @@
-public class GuestList {
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Objects;
+
+@AllArgsConstructor
+public class GuestList{
+
+    public ArrayList<String> guests;
+
+    public GuestList() {
+
+    }
+
+   public void setGuests(ArrayList<String> guestList) throws IOException {
+        this.guests = guestList;
+        Path filePath = Paths.get("src/main/java/guests.txt");
+        Files.write(filePath, guestList);
+   }
+   public ArrayList<String> getGuests() throws IOException {
+        if (this.guests != null) {
+            return this.guests;
+        }
+        if (this.guests == null) {
+            if (filePathExists(Paths.get("src/main/java/guests.txt"))) {
+                return (ArrayList<String>) Files.readAllLines(Paths.get("src/main/java/guests.txt"));
+            }
+        }
+        return null;
+   }
+
+
+   public boolean filePathExists(Path path) {
+        return Files.exists(path);
+   }
+
+    @Override
+    public String toString() {
+        return "GuestList{" +
+                "guests=" + guests +
+                '}';
+    }
+
 }
--- src/test/java/GuestListTest.java
+++ src/test/java/GuestListTest.java
@@ -1,13 +1,18 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuestListTest {
      @Test
-     void shouldBeEmptyInitially() {
+     void shouldBeEmptyInitially() throws IOException {
           //Given
           GuestList newGuestList = new GuestList();
           //When
@@ -18,7 +23,7 @@ class GuestListTest {
      }
 
      @Test
-     void shouldReadSameGuestsAsWrittenBefore() {
+     void shouldReadSameGuestsAsWrittenBefore() throws IOException {
           //Given
           GuestList newGuestList = new GuestList();
           //When
@@ -30,6 +35,38 @@ class GuestListTest {
           //Then
           Assertions.assertEquals(guestList, actual);
      }
+
+     @Test
+     void shouldWriteToFileSystem() throws IOException {
+          //Given
+          GuestList newGuestList = new GuestList();
+          //When
+          ArrayList<String> guestList = new ArrayList<>();
+          guestList.add("Theodor");
+          guestList.add("Anette");
+          newGuestList.setGuests(guestList);
+          ArrayList<String> actual = (ArrayList<String>) Files.readAllLines(Path.of("src/main/java/guests.txt"));
+          //Then
+          Assertions.assertEquals(guestList, actual);
+     }
+
+     @Test
+     void shouldReadFromFileSystem() throws IOException {
+          //Given
+          ArrayList<String> guestList = new ArrayList<>();
+          guestList.add("Stephan");
+          guestList.add("Max");
+          Path filePath = Paths.get("src/main/java/guests.txt");
+          Files.write(filePath, guestList);
+          //When
+          GuestList newGuestList = new GuestList();
+          ArrayList<String> actual = newGuestList.getGuests();
+          //Then
+          Assertions.assertEquals(guestList, actual);
+
+     }
+
+
 
 
 
